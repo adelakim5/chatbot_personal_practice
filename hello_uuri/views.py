51844e9d621300881efb644c36549f831cdc7697
adelakim <diddnjs02@likelion.org>
2020-01-10 01:13:08 +0900

    delete if

--- hello_uuri/views.py
+++ hello_uuri/views.py
@@ -31,20 +31,11 @@ def hello(request):
         else:
             answer = 3
         # 동작하는 사용자가 기존에 사용한 적 있는 user인지 아닌지 검사 
-        user = User.objects.get(userId=userId)
-        # 만약 user라면 
-        if user:
-            # 기존에 작성된 answer들 다 새로 저장 
-            question = Question.objects.all().filter(userId=user).filter(answer=block_name)
-            # 만약 question이 있으묜
-            if question:
-                question[0].answer = answer
-                question[0].save()
-        else:
-            user = User(userId=userId)
-            user.save()
-            question = Question(question=block_name, answer=answer, userId=user)
-            question.save()
+        # 만약 user아니면 
+        user = User(userId=userId)
+        user.save()
+        question = Question(question=block_name, answer=answer, userId=user)
+        question.save()
         # 응답 보내주기
         data = transformData(block_id, userId).getJsonData()
     else:
